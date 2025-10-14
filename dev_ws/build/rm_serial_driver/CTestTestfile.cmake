1a321e5edaae324ddcddd8930616fabbffb16aaf
lucaschang1918 <lucaschang1918@gmail.com>
2025-10-14 22:32:50 +0800

    perfect

--- dev_ws/build/rm_serial_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rm_autoaim/suep2026/dev_ws/src/rm_serial_driver
-# Build directory: /home/rm_autoaim/suep2026/dev_ws/build/rm_serial_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
